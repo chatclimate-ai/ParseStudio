20769ace8c60af8b7f4f17798f2015b0f2991073
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2024-12-03 11:16:42 +0100

    docstrings for mkdocs

--- parsestudio/parsers/pymupdf_parser.py
+++ parsestudio/parsers/pymupdf_parser.py
@@ -17,6 +17,13 @@ class PyMuPDFParser:
     @staticmethod
     def load_documents(paths: List[str]) -> Generator[List[Page], None, None]:
         """
+        Load the documents from the given paths.
+
+        Args:
+            paths (List[str]): List of paths to the PDF files.
+        
+        Returns:
+            result (Generator[List[Page], None, None]): A generator that yields a list of pages for each document
         """
         for path in paths:
             with fitz.open(path) as doc:
@@ -25,6 +32,13 @@ class PyMuPDFParser:
 
     def _validate_modalities(self, modalities: List[str]) -> None:
         """
+        Validate the modalities provided by the user. The valid modalities are: ["text", "tables", "images"]
+
+        Args:
+            modalities (List[str]): List of modalities to validate
+
+        Raises:
+            ValueError: If the modality is not valid
         """
         valid_modalities = ["text", "tables", "images"]
         for modality in modalities:
@@ -39,6 +53,51 @@ class PyMuPDFParser:
         modalities: List[str] = ["text", "tables", "images"],
     ) -> List[ParserOutput]:
         """
+        Parse the PDF file and return the extracted the specified modalities.
+
+        Args:
+            paths (Union[str, List[str]]): A path or a list of paths to the PDF files.
+            modalities (List[str], optional): List of modalities to extract. Defaults to ["text", "tables", "images"].
+        
+        Returns:
+            data (List[ParserOutput]): A list of ParserOutput objects containing the extracted modalities.
+        
+        Raises:
+            ValueError: If the modality is not valid
+        
+        Example:
+        !!! example
+            ```python
+            parser = PyMuPDFParser()
+            data = parser.parse("path/to/file.pdf", modalities=["text", "tables", "images"])
+            print(len(data)) 
+            # Output: 1
+            text = data[0].text # TextElement
+            tables = data[0].tables # List of TableElement
+            images = data[0].images # List of ImageElement
+
+            # Access the text
+            text = text.text
+
+
+            # Access the first table
+            table = tables[0]
+            # Access the markdown representation of the table
+            table_md = table.markdown
+            # Access the dataframe representation of the table
+            table_df = table.dataframe
+            # Access the metadata of the table
+            page_number = table.metadata.page_number
+            bbox = table.metadata.bbox
+
+            # Access the first image
+            image = images[0]
+            # Access the image object
+            image_obj = image.image # PIL Image object
+            # Access the metadata of the image
+            page_number = image.metadata.page_number
+            bbox = image.metadata.bbox 
+            ```
         """
         self._validate_modalities(modalities)
 
@@ -55,6 +114,14 @@ class PyMuPDFParser:
 
     def __export_result(self, pages: List[Page], modalities: List[str]) -> ParserOutput:
         """
+        Export the result of the parsing process.
+
+        Args:
+            pages (List[Page]): List of pages
+            modalities (List[str]): List of modalities to extract
+        
+        Returns:
+            output (ParserOutput): The ParserOutput object containing the extracted modalities.
         """
         text = TextElement(text="")
         tables: List[TableElement] = []
@@ -75,12 +142,51 @@ class PyMuPDFParser:
     @staticmethod
     def _extract_text(page: Page) -> TextElement:
         """
+        Extract the text from the page.
+
+        Args:
+            page (Page): The page object
+
+        Returns:
+            text (TextElement): The extracted text element
+        
+        Example:
+        !!! example
+            ```python
+            parser = PyMuPDFParser()
+            with fitz.open("path/to/file.pdf") as doc:
+                page = doc.load_page(0)
+                text = parser._extract_text(page)
+                print(text.text)
+            
+            # Output: 'Hello, World!'
+            ```
         """
         return TextElement(text=page.get_text("text"))
 
     @staticmethod
     def _extract_images(page: Page) -> List[ImageElement]:
         """
+        Extract the images from the page.
+
+        Args:
+            page (Page): The page object
+
+        Returns:
+            images (List[ImageElement]): List of ImageElement objects
+
+        Example:
+        !!! example
+            ```python
+            parser = PyMuPDFParser()
+            with fitz.open("path/to/file.pdf") as doc:
+                page = doc.load_page(0)
+                images = parser._extract_images(page)
+                image = images[0]
+                image_obj = image.image
+                page_number = image.metadata.page_number
+                bbox = image.metadata.bbox
+            ```
         """
         images: List[ImageElement] = []
         for img in page.get_images(full=True):
@@ -94,6 +200,27 @@ class PyMuPDFParser:
     @staticmethod
     def _extract_tables(page: Page) -> List[TableElement]:
         """
+        Extract the tables from the page.
+
+        Args:
+            page (Page): The page object
+
+        Returns:
+            tables (List[TableElement]): List of TableElement objects
+
+        Example:
+        !!! example
+            ```python
+            parser = PyMuPDFParser()
+            with fitz.open("path/to/file.pdf") as doc:
+                page = doc.load_page(0)
+                tables = parser._extract_tables(page)
+                table = tables[0]
+                table_md = table.markdown
+                table_df = table.dataframe
+                page_number = table.metadata.page_number
+                bbox = table.metadata.bbox
+            ```
         """
         tabs = page.find_tables()
 
